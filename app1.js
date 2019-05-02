78d0b773a734710969cf7d51546c82bee795a7f1
root <root@localhost.localdomain>
2019-05-02 07:40:29 +0545

    update

--- app1.js
+++ app1.js
@@ -1,8 +1,39 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const app =express();
 
-const Student = require('express');
+const connectDb = require("./database.js")
 
-const app = express();
-const port = 3000;
+const schema=require("./schema.js")
 
-app.get('/', function (request, response))
+const Blog = mongoose.model('Blog', schema);
+const blog = new Blog({title:"democracy day",author:"Ram",body:"32"});
+    blog.save(function (err,saveblog){
+    if (err) response.send({error:err.message});
+        console.log(saveblog);
+    })
+
+ app.get('/blogs',function(request, response){
+    Blog.find(function (err, listedBlog) {
+        if (err) response.send({error:err.message});
+        response.send(listedBlog);
+      })
+})
+app.post('/blogs',function(request,response){
+    const blog=request.body;
+    Blog.create(data,(err,createData)=>{
+        if(err) response.send({error:err.message})
+        response.send(createData)
+    })
+    response.send('created user')
+})
+
+
+connectDb
+.on('error', function (err) { console.log(err) })
+.on('connected', function () {
+    app.listen(3000,function(){
+        console.log('running at port 3000')
+    } )
+})
+.on('disconnected', () => connectDb)
